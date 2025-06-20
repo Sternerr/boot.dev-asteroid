465673974ef1f1bf2691545c9265ce05a507c43e
Noah <volpy2000@gmail.com>
2025-06-20 15:01:22 +0200

    added game clock

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
     
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -16,6 +19,9 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        elapsed_ms = clock.tick(60)
+        dt = elapsed_ms / 1000.0
 
 if __name__ == "__main__":
         main()
